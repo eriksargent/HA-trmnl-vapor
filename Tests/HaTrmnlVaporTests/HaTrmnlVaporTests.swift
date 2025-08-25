477f276fc1e5596f35d720a485964d6586e81c0a
Erik Sargent <admin@eriksargent.me>
2025-08-25 14:01:36 -0600

    Adding docker to dependabot

--- Tests/HaTrmnlVaporTests/HaTrmnlVaporTests.swift
+++ Tests/HaTrmnlVaporTests/HaTrmnlVaporTests.swift
@@ -4,12 +4,11 @@ import Testing
 
 @Suite("App Tests")
 struct HaTrmnlVaporTests {
-    @Test("Test Hello World Route")
-    func helloWorld() async throws {
+    @Test("Test main route")
+    func mainRoute() async throws {
         try await withApp(configure: configure) { app in
-            try await app.testing().test(.GET, "hello", afterResponse: { res async in
+            try await app.testing().test(.GET, "", afterResponse: { res async in
                 #expect(res.status == .ok)
-                #expect(res.body.string == "Hello, world!")
             })
         }
     }
