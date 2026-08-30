cb6d4e5d404848cd166dc6e89fc7dae3e6c6cc9c
Erik Sargent <admin@eriksargent.me>
2026-08-30 11:38:47 -0600

    fix: handle unavailable states

--- Sources/HaTrmnlVapor/trmnl.swift
+++ Sources/HaTrmnlVapor/trmnl.swift
@@ -30,41 +30,41 @@ struct OverviewSlide: TrmnlContent {
 	static func makeFromStates(_ states: [HomeAssistant.State]) throws -> OverviewSlide {
 		Self.init(
 			mainstogridEnergyToday:
-				try states.entity(id: "sensor.mainstogrid_energy_today")?
-				.number(precision: 2, addUnit: "kWh") ?? "",
+				(try? states.entity(id: "sensor.mainstogrid_energy_today")?
+				.number(precision: 2, addUnit: "kWh")) ?? "",
 			mainsfromgridEnergyToday:
-				try states.entity(id: "sensor.mainsfromgrid_energy_today")?
-				.number(precision: 2, addUnit: "kWh") ?? "",
+				(try? states.entity(id: "sensor.mainsfromgrid_energy_today")?
+				.number(precision: 2, addUnit: "kWh")) ?? "",
 			inverterOutput:
-				try states.entity(id: "sensor.total_inverter_grid_power")?
-				.number(precision: 2, scale: 1.0 / 1000, addUnit: "kW") ?? "",
+				(try? states.entity(id: "sensor.total_inverter_grid_power")?
+				.number(precision: 2, scale: 1.0 / 1000, addUnit: "kW")) ?? "",
 			totalSolarDailyYield:
-				try states.entity(id: "sensor.total_solar_daily_yield")?
-				.number(precision: 2, scale: 1.0 / 1000, addUnit: "kWh") ?? "",
+				(try? states.entity(id: "sensor.total_solar_daily_yield")?
+				.number(precision: 2, scale: 1.0 / 1000, addUnit: "kWh")) ?? "",
 			batteryPercentage:
-				try states.entity(id: "sensor.sonnenbatterie_156865_state_battery_percentage_user")?
-				.number(precision: 0, addUnit: "%") ?? "",
+				(try? states.entity(id: "sensor.sonnenbatterie_156865_state_battery_percentage_user")?
+				.number(precision: 0, addUnit: "%")) ?? "",
 			batteryState:
 				states.entity(id: "sensor.sonnenbatterie_156865_state_sonnenbatterie")?
 				.capitalized() ?? "",
 			outdoorHumidity:
-				try states.entity(id: "sensor.weather_station_outdoor_module_humidity")?
-				.number(precision: 0, addUnit: "%") ?? "",
+				(try? states.entity(id: "sensor.weather_station_outdoor_module_humidity")?
+				.number(precision: 0, addUnit: "%")) ?? "",
 			outdoorTemperature:
-				try states.entity(id: "sensor.weather_station_outdoor_module_temperature")?
-				.number(precision: 1, addUnit: "ºF") ?? "",
+				(try? states.entity(id: "sensor.weather_station_outdoor_module_temperature")?
+				.number(precision: 1, addUnit: "ºF")) ?? "",
 			windGustStrength:
-				try states.entity(id: "sensor.weather_station_smart_anemometer_gust_strength")?
-				.number(precision: 1, addUnit: "mph") ?? "",
+				(try? states.entity(id: "sensor.weather_station_smart_anemometer_gust_strength")?
+				.number(precision: 1, addUnit: "mph")) ?? "",
 			windSpeed:
-				try states.entity(id: "sensor.weather_station_smart_anemometer_wind_speed")?
-				.number(precision: 1, addUnit: "mph") ?? "",
+				(try? states.entity(id: "sensor.weather_station_smart_anemometer_wind_speed")?
+				.number(precision: 1, addUnit: "mph")) ?? "",
 			rainLastHour:
-				try states.entity(id: "sensor.weather_station_smart_rain_gauge_precipitation_last_hour")?
-				.number(precision: 3, addUnit: "in") ?? "",
+				(try? states.entity(id: "sensor.weather_station_smart_rain_gauge_precipitation_last_hour")?
+				.number(precision: 3, addUnit: "in")) ?? "",
 			rainToday:
-				try states.entity(id: "sensor.weather_station_smart_rain_gauge_precipitation_today")?
-				.number(precision: 3, addUnit: "in") ?? ""
+				(try? states.entity(id: "sensor.weather_station_smart_rain_gauge_precipitation_today")?
+				.number(precision: 3, addUnit: "in")) ?? ""
 		)
 	}
 }
@@ -90,33 +90,33 @@ struct PowerSlide: TrmnlContent {
 		
 		return Self.init(
 			currentInverterPower:
-				try states.entity(id: "sensor.total_inverter_grid_power")?
-				.double(scale: 1.0 / 1000) ?? 0,
+				(try? states.entity(id: "sensor.total_inverter_grid_power")?
+				.double(scale: 1.0 / 1000)) ?? 0,
 			maxInterverPower:
-				try states.entity(id: "sensor.total_inverter_grid_power_daily_max")?
-				.double(scale: 1.0 / 1000) ?? 0,
+				(try? states.entity(id: "sensor.total_inverter_grid_power_daily_max")?
+				.double(scale: 1.0 / 1000)) ?? 0,
 			batteryPercentage:
-				Int(try states.entity(id: "sensor.sonnenbatterie_156865_state_battery_percentage_user")?
-					.double() ?? 0),
+				Int((try? states.entity(id: "sensor.sonnenbatterie_156865_state_battery_percentage_user")?
+					.double()) ?? 0),
 			batteryState:
 				states.entity(id: "sensor.sonnenbatterie_156865_state_sonnenbatterie")?
 				.capitalized() ?? "",
 			totalSolarDailyYield:
-				try states.entity(id: "sensor.total_solar_daily_yield")?
-				.double(scale: 1.0 / 1000) ?? 0,
+				(try? states.entity(id: "sensor.total_solar_daily_yield")?
+				.double(scale: 1.0 / 1000)) ?? 0,
 			homeToGridEnergyToday:
-				try states.entity(id: "sensor.mainstogrid_energy_today")?
-				.double() ?? 0,
+				(try? states.entity(id: "sensor.mainstogrid_energy_today")?
+				.double()) ?? 0,
 			leftSolarEnergyToday:
-				try states.entity(id: "sensor.left_battery_inverter_daily_yield")?
-				.double(scale: 1.0 / 1000) ?? 0,
+				(try? states.entity(id: "sensor.left_battery_inverter_daily_yield")?
+				.double(scale: 1.0 / 1000)) ?? 0,
 			rightSolarEnergyToday:
-				try states.entity(id: "sensor.right_inverter_daily_yield")?
-				.double(scale: 1.0 / 1000) ?? 0,
+				(try? states.entity(id: "sensor.right_inverter_daily_yield")?
+				.double(scale: 1.0 / 1000)) ?? 0,
 			gridToBatteryEnergyToday: batteryGridIn,
 			gridToHomeEnergyToday: homeGridIn,
-			batteryToHomeEnergyToday: try states.entity(id: "sensor.battery_microgrid_energy_today")?
-				.double(scale: 1.0 / 1000) ?? 0
+			batteryToHomeEnergyToday: (try? states.entity(id: "sensor.battery_microgrid_energy_today")?
+				.double(scale: 1.0 / 1000)) ?? 0
 		)
 	}
 }
